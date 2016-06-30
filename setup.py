3aa57b52fa49045e646192241402d5da05311f4c
penta515 <yamada@serverworks.co.jp>
2016-06-30 22:21:15 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import codecs
+import os
+from setuptools import setup
+
+dirname = os.path.dirname(__file__)
+
+long_description = (
+    codecs.open(os.path.join(dirname, 'README.md'), encoding='utf-8').read()
+)
+
+setup(
+    name='slash-command-decorator',
+    version='0.0.1',
+    description='Useful Slack Slash Command decorators for AWS Lambda.',
+    author='penta515',
+    author_email='penta0515@gmail.com',
+    url='https://github.com/penta515/slash-command-decorator',
+    packages=['slash_command_decorator'],
+    install_requires=['slacker']
+)
